9eca54ca1c877ad812d334733af105d863a4ee69
Kyle <kfranke1@binghamton.edu>
2020-04-19 23:12:28 -0400

    Lift up state from board to game, finish time travel

--- src/index.js
+++ src/index.js
@@ -13,53 +13,81 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            squares: Array(9).fill(null),
-            isVictory: false,
-            turn: 'X'
-        }
-
-        this.moves = [];
-    }
-
     /* Renders each square, takes note of it in an array,
      * and sets its on-click function.
      */
     renderSquare(i) {
-        return <Square 
-                    value={this.state.squares[i]}
-                    onClick={() => this.handleSquareClick(i)}
-                />;
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
     }
 
+    render() {
+        return (
+            <div onClick={this.boardOnClick}>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            moves: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ],
+            isVictory: false,
+            display: 'X'
+        }
+    }
+    
     /* Handles click when a square is pressed. Note that this
      * is defined within the Board class, not the Square class.
      */
     handleSquareClick(i) {
-        const squares = this.state.squares.slice();
-        const curPlayer = this.state.turn;
+        const moves = this.state.moves;
+        const current = moves[moves.length - 1];
+        const squares = current.squares.slice();
+        const curPlayer = this.state.display;
 
         if (squares[i] === null && !this.state.isVictory) {
             squares[i] = curPlayer;
             
             const didWin = this.checkVictory(squares, curPlayer);
-            const nextTurn = (didWin ^ curPlayer === 'X') ?
+            const display = (didWin ^ curPlayer === 'X') ?
                              'O' : 'X';
             this.setState(
                 {
-                    squares: squares,
+                    moves: moves.concat([
+                        {
+                            squares: squares,
+                        }
+                    ]),
                     isVictory: didWin, 
-                    turn: nextTurn
+                    display: display 
                 });
-
-            this.moves.push([curPlayer, i]);
-        }
-
-        for (let x of this.moves) {
-            console.log(x);
         }
     }
 
@@ -67,7 +95,6 @@ class Board extends React.Component {
      * previous move.
      */
     checkVictory(squares, ch) {
-        console.log(squares);
         for (var i = 0; i < 3; i++) {
             // check column i
             if (squares[i] === ch &&
@@ -93,44 +120,44 @@ class Board extends React.Component {
                 squares[6] === ch);
     }
 
-    render() {
-        return (
-            <div onClick={this.boardOnClick}>
-                <div className="status">
-                    {(this.state.isVictory) ?
-                        ("The winner is " + this.state.turn) :
-                        ("Next player: " + this.state.turn)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+    jumpTo(i) {
+        const cutMoves = this.state.moves.slice(0, i + 1);
+        this.setState({
+            moves: cutMoves,
+            isVictory: false,
+            display: (i % 2 === 0) ? 'X' : 'O'
+        });
     }
-}
 
-class Game extends React.Component {
     render() {
+        const moves = this.state.moves;
+        const current = moves[moves.length - 1];
+       
+        const historyButtons = moves.map((step, move) => {
+            const text = move === 0 ?
+                "Go to game start" :
+                "Go to move #" + move;
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {text}
+                    </button>
+                </li>
+            );
+        });
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current.squares}
+                           onClick={(i) => this.handleSquareClick(i)}/>
                 </div>
             <div className="game-info">
-                <div>Playboi Carti</div>
-                    <ol>{/* TODO */}</ol>
+                <div>{(this.state.isVictory ? 
+                        "Winner: " : "Next player: ") + 
+                      this.state.display}</div>
+                    <ol>{historyButtons
+                            .slice(0, historyButtons.length - 1)}</ol>
                 </div>
             </div>
         );
